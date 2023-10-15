6ab1329bb49a4f594b92feb5eb765bc6ae77d214
fkamag <fabiokamagian@gmail.com>
2023-10-15 15:23:44 -0300

    exercicios 1 e 2

---
+++ src/Exercicio2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite um número: ");
+    var num = scanner.nextInt();
+
+    System.out.println("O número informado foi " + num);
+
+  }
+}
