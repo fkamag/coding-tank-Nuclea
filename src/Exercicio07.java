e0b66d302c79defcd926d5fcccf2f6a81a90a3f8
fkamag <fabiokamagian@gmail.com>
2023-10-15 19:43:40 -0300

    exercicio07

---
+++ src/Exercicio07.java
@@ -0,0 +1,14 @@
+public class Exercicio07 {
+
+  public static void main(String[] args) {
+
+    float frente = Float.parseFloat(args[0]);
+
+    double resultado = Math.pow(frente, 2) * 2;
+
+    System.out.println("O dobro da área do quadrado é " + resultado);
+
+
+  }
+
+}
