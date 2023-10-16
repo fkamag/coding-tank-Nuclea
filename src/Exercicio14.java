38aa008df823a96bd4c17c6670cf0b1d0eeff9d6
fkamag <fabiokamagian@gmail.com>
2023-10-16 16:29:43 -0300

    exercicio14

---
+++ src/Exercicio14.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio14 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite a quantidade de peixes em KG: ");
+    float peso = scanner.nextFloat();
+
+    if (peso > 50) {
+      float excesso = peso - 50;
+      float multa = excesso * 4;
+
+      System.out.printf("O excesso de peso foi de %.3f Kg "
+          + "e o valor da multa ficou em R$ %.2f", excesso, multa);
+    }
+  }
+
+}
