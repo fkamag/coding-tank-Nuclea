1e9af6e9874d97e73c424eb6dd31fd159a12d487
fkamag <fabiokamagian@gmail.com>
2023-10-16 17:04:20 -0300

    exercicio16

---
+++ src/Exercicio16.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio16 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a quantidade de m² que precisa pintar: ");
+    float area = scanner.nextInt();
+
+    int qtdeLatas = 0;
+
+    do {
+      qtdeLatas ++;
+    } while (qtdeLatas * 54 < area);
+
+    float valorTotal = qtdeLatas * 80;
+
+    System.out.println("A quantidade para atender a área é de " + qtdeLatas + " latas.");
+    System.out.printf("O valor total é de R$ %.2f", valorTotal);
+
+  }
+
+}
