30ae2073677e42990ab75dcde9c95fc42eaa0b02
fkamag <fabiokamagian@gmail.com>
2023-10-16 18:19:47 -0300

    exercicio17

---
+++ src/Exercicio18.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite o tamanho do arquivo em MB: ");
+    float tamanhoArquivo = scanner.nextFloat();
+    System.out.print("Digite a velocidade da conexão em Mbps: ");
+    float velocidade = scanner.nextFloat();
+
+    float tempo = (tamanhoArquivo / (velocidade / 8)) / 60;
+
+    System.out.printf("O tempo aproximado é de %.0f minutos", tempo);
+
+  }
+
+}
