a5a2855f17a4eaf857d37d6021f57c001120aac7
fkamag <fabiokamagian@gmail.com>
2023-10-17 18:50:57 -0300

    exercicio 3 lista 2

---
+++ src/lista2/Exercicio03.java
@@ -0,0 +1,22 @@
+package lista2;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite 'F' para Feminino e 'M' para Masculino: ");
+    var sexo = scanner.next();
+
+    if (sexo.equals("F")) {
+      System.out.println("Feminino");
+    } else if (sexo.equals("M")) {
+      System.out.println("Masculino");
+    } else {
+      System.out.println("Sexo Inválido");
+    }
+  }
+
+}
