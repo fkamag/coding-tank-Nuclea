e286464fc8bbdadb9fe90e521d04d806fac3670f
fkamag <fabiokamagian@gmail.com>
2023-10-19 23:49:39 -0300

    exercicio 17 lista 3

---
+++ src/lista3/Exercicio17.java
@@ -0,0 +1,23 @@
+package lista3;
+
+import java.util.Scanner;
+
+public class Exercicio17 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Cálculo de Fatoria. Digite um número: ");
+    int numero = scanner.nextInt();
+    String mensagem = numero + "!=";
+    int total = 1;
+
+    for (int i = numero; i >= 2; i--) {
+      total *= i;
+      mensagem += i + ".";
+    }
+    mensagem += "1=" + total;
+    System.out.println(mensagem);
+  }
+
+}
